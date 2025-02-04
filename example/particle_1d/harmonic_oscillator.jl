f5578c9f5b57d5d12bcc6960ddb675050a31c769
Leonardo Galliano <leonardo.galliano@phd.units.it>
2025-02-04 11:37:07 +0100

    Add particle 1D example

---
+++ example/particle_1d/harmonic_oscillator.jl
@@ -0,0 +1,44 @@
+# EXAMPLE: Harmonic Oscillator
+include("particle_1d.jl")
+
+potential(x) = x^2
+###############################################################################
+
+## RUN THE SIMULATION
+seed = 42
+rng = Xoshiro(seed)
+β = 2.0
+M = 10
+chains = [Particle(4rand(rng) - 2, β) for _ in 1:M]
+pools = [(Move(Displacement(0.0), StandardGaussian(), ComponentArray(σ=0.1), 1.0),) for _ in 1:M]
+steps = 10^5
+burn = 1000
+block = [0, 10]
+sampletimes = scheduler(steps, burn, block)
+path = "data/particle_1d/Harmonic/beta$β/M$M/seed$seed"
+simulation = Simulation(chains, pools, steps; sampletimes=sampletimes, seed=seed, parallel=false, verbose=true, store_trajectory=true, path=path)
+callbacks = (callback_energy, callback_acceptance)
+run!(simulation, callbacks...)
+
+## PLOT RESULTS
+using Plots, Statistics, Measures, DelimitedFiles
+default(tickfontsize=15, guidefontsize=15, titlefontsize=15, legendfontsize=15,
+    grid=false, size=(500, 500), minorticks=5)
+
+energies = readdlm(joinpath(path, "energy.dat"))[:, 2]
+@show mean(energies), std(energies)
+
+target_density(x, β) = exp(-β * x^2) * sqrt(β / pi)
+xx = LinRange(-2.0, 2.0, 1000)
+target = target_density.(xx, β)
+plot(xlabel="x", ylabel="p(x)", title="β=$β", legend=:bottomright)
+plot!(xx, target, lw=3, label="Target density", c=:red)
+
+trj_files = [joinpath(dir, "trajectory.xyz") for dir in readdir(joinpath(path, "trajectories"), join=true)]
+trajectories = map(file -> readdlm(file)[:, 2], trj_files)
+positions = vcat(trajectories...)
+
+stephist!(positions, normalize=:pdf, lw=3, label="Simulation", c=1)
+savefig("example/particle_1d/harmonic_oscillator_density.png")
+
+
---
+++ example/particle_1d/particle_1d.jl
@@ -0,0 +1,73 @@
+using MonteCarlo
+using Random
+using Distributions
+using ComponentArrays
+
+###############################################################################
+## SYSTEM
+mutable struct Particle{T<:AbstractFloat}
+    x::T
+    β::T
+    e::T
+    function Particle(x::T, β::T) where {T<:AbstractFloat}
+        return new{T}(x, β, potential(x))
+    end
+end
+
+System(x, β) = Particle(x, β)
+
+function MonteCarlo.delta_log_target_density(e₁, e₂, system)
+    return -system.β * (e₂ - e₁)
+end
+
+###############################################################################
+## ACTIONS
+mutable struct Displacement{T<:AbstractFloat} <: Action
+    δ::T
+end
+
+function MonteCarlo.perform_action!(system::Particle, action::Displacement)
+    e₁ = system.e
+    system.x += action.δ
+    system.e = potential(system.x)
+    return e₁, system.e
+end
+
+function MonteCarlo.invert_action!(action::Displacement, system::Particle)
+    action.δ = -action.δ
+    return nothing
+end
+
+function reward(action::Displacement, system::Particle)
+    return (action.δ)^2
+end
+
+###############################################################################
+## POLICIES
+struct StandardGaussian <: Policy end
+
+setup_parameters(::StandardGaussian) = ComponentArray(σ=1.0)
+
+function MonteCarlo.log_proposal_density(action::Displacement, ::StandardGaussian, parameters, system::Particle)
+    return -(action.δ)^2 / (2parameters.σ^2) - log(2π * parameters.σ^2) / 2
+end
+
+function MonteCarlo.sample_action!(action::Displacement, ::StandardGaussian, parameters, system::Particle, rng)
+    action.δ = rand(rng, Normal(zero(action.δ), parameters.σ))
+    return nothing
+end
+
+###############################################################################
+## UTILS
+function MonteCarlo.store_trajectory(trj, system::Particle, t::Int)
+    println(trj, "$t $(system.x)")
+    return nothing
+end
+
+function callback_energy(simulation)
+    return mean(system.e for system in simulation.chains)
+end
+
+###############################################################################
+
+nothing
