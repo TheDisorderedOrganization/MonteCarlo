c005ee8be0a03ee9eb1af144a926717155221dba
Leonardo Galliano <leonardo.galliano@phd.units.it>
2025-02-25 16:20:00 +0100

    Improve ordering in module

--- src/MonteCarlo.jl
+++ src/MonteCarlo.jl
@@ -13,18 +13,19 @@ using Transducers
 using Dates
 using Printf
 
-include("simulation.jl")
-include("algorithms.jl")
 include("metropolis.jl")
-
 export Action, Policy, Move
 export sample_action!, perform_action!, perform_action_cached!, invert_action!
 export log_proposal_density, delta_log_target_density
 export mc_step!, mc_sweep!
 export Metropolis, callback_acceptance, StoreParameters
-export build_schedule, StoreCallbacks, StoreTrajectories, StoreLastFrames, StoreBackups, PrintTimeSteps
-export Simulation, run!
+
+include("algorithms.jl")
+export StoreCallbacks, StoreTrajectories, StoreLastFrames, StoreBackups, PrintTimeSteps
 export TXT, DAT
+
+include("simulation.jl")
+export Simulation, build_schedule, run!
 
 include("PolicyGuided/PolicyGuided.jl")
 
