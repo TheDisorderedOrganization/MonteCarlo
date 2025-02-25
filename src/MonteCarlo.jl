54876a89c59baa4231b3243963fe4ed1d25f0c6c
Leonardo Galliano <leonardo.galliano@phd.units.it>
2025-02-25 16:23:49 +0100

    Fix mistake in ordering

--- src/MonteCarlo.jl
+++ src/MonteCarlo.jl
@@ -13,19 +13,19 @@ using Transducers
 using Dates
 using Printf
 
+include("simulation.jl")
+export Simulation, build_schedule, run!
+
+include("algorithms.jl")
+export Algorithm, StoreCallbacks, StoreTrajectories, StoreLastFrames, StoreBackups, PrintTimeSteps
+export TXT, DAT
+
 include("metropolis.jl")
 export Action, Policy, Move
 export sample_action!, perform_action!, perform_action_cached!, invert_action!
 export log_proposal_density, delta_log_target_density
 export mc_step!, mc_sweep!
 export Metropolis, callback_acceptance, StoreParameters
-
-include("algorithms.jl")
-export StoreCallbacks, StoreTrajectories, StoreLastFrames, StoreBackups, PrintTimeSteps
-export TXT, DAT
-
-include("simulation.jl")
-export Simulation, build_schedule, run!
 
 include("PolicyGuided/PolicyGuided.jl")
 
