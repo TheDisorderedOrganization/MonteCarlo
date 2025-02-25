c11111a0323475f8238833b5d5ebb7308ac305ab
Leonardo Galliano <107858186+leonardogalliano@users.noreply.github.com>
2025-02-25 16:36:21 +0100

    Minor changes (#53)
    * Add store_lastframe and store_backup functions
    
    * Separate utils file into algorithms and simulation
    
    * Separate utils file into algorithms and simulation
    
    * Improve ordering in module
    
    * Fix mistake in ordering
    
    ---------
    
    Co-authored-by: Leonardo Galliano <leonardo.galliano@phd.units.it>

--- src/MonteCarlo.jl
+++ src/MonteCarlo.jl
@@ -14,17 +14,18 @@ using Dates
 using Printf
 
 include("simulation.jl")
-include("utils.jl")
-include("metropolis.jl")
+export Simulation, build_schedule, run!
 
+include("algorithms.jl")
+export Algorithm, StoreCallbacks, StoreTrajectories, StoreLastFrames, StoreBackups, PrintTimeSteps
+export TXT, DAT
+
+include("metropolis.jl")
 export Action, Policy, Move
 export sample_action!, perform_action!, perform_action_cached!, invert_action!
 export log_proposal_density, delta_log_target_density
 export mc_step!, mc_sweep!
 export Metropolis, callback_acceptance, StoreParameters
-export build_schedule, StoreCallbacks, StoreTrajectories, StoreLastFrames, StoreBackups, PrintTimeSteps
-export Simulation, run!
-export TXT, DAT
 
 include("PolicyGuided/PolicyGuided.jl")
 
